2c214e661747390539d80977e6b4cb90b0d6f020
Tanso <tansomctexp@gmail.com>
2025-01-25 14:27:47 +0900

    [UPDATE]Deleted files.

--- CuRendCore/CuRendCore/include/CuRendCore.h
+++ CuRendCore/CuRendCore/include/CuRendCore.h
@@ -3,32 +3,3 @@
 #include <memory>
 
 #include "CRCConfig.h"
-
-#include "Window.h"
-#include "Scene.h"
-#include "Resource.cuh"
-#include "Binder.h"
-
-namespace CRC 
-{
-
-class CRC_API CuRendCore 
-{
-private:
-    CuRendCore(); // Singleton pattern.
-
-public:
-    ~CuRendCore();
-
-    static CuRendCore* GetInstance();
-    void ReleaseInstance();
-
-    // Various factories. Managed collectively in this class.
-    WindowFactory* windowFc;
-    SceneFactory* sceneFc;
-    ResourceFactory* resourceFc;
-
-    int Run(HINSTANCE hInstance, int nCmdShow);
-};
-
-} // namespace CRC
